133ee6386f93ef23c5d21ab7552378282a326849
tikhtmp <57909756+tikhtmp@users.noreply.github.com>
2021-12-29 16:41:59 +0300

    Commit test

--- src/main/java/by/karas/Main.java
+++ src/main/java/by/karas/Main.java
@@ -1,2 +1,12 @@
-package by.karas;public class Main {
+package by.karas;
+
+public class Main {
+    public static void main(String[] args) {
+        double d = 10;
+        double pr1 = 20.05;
+        double w = 30;
+        double pr2 = 30.5;
+        double result = d * pr1 + w * pr2;
+        System.out.println("Result = " + result); // new
+    }
 }
