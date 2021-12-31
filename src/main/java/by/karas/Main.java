42a9602939db061dc3627b565eed32d0c8e1f617
tikhtmp <57909756+tikhtmp@users.noreply.github.com>
2021-12-31 15:11:44 +0300

    User dialog:

--- src/main/java/by/karas/Main.java
+++ src/main/java/by/karas/Main.java
@@ -1,12 +1,58 @@
 package by.karas;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        double d = 10;
-        double pr1 = 20.05;
-        double w = 30;
-        double pr2 = 30.5;
-        double result = d * pr1 + w * pr2; // comments1
-        System.out.println("Result = " + result);
+
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        String inputValue;
+        Double[] enteredValues = new Double[4];
+
+        //File for prices
+        // Change calc function
+        // Change user dialogs
+
+        do {
+            if (i == 0) {
+                System.out.println("Please, enter distance: ");
+            } else if (i == 1) {
+                System.out.println("Please, enter price per km: ");
+            } else if (i == 2) {
+                System.out.println("Please, enter weight: ");
+            } else if (i == 3) {
+                System.out.println("Please, enter price per kg: ");
+            }
+            inputValue = scanner.next();
+
+            if (inputValue.equalsIgnoreCase("q")) {
+                break;
+            } else if(isCorrectDoubleValue(inputValue)){
+                enteredValues[i] = Double.parseDouble(inputValue);
+                i++;
+            } else {
+                System.out.println("Incorrect value: " + inputValue);
+            }
+
+            if (i == 4) {
+                Double result = enteredValues[0] * enteredValues[1] + enteredValues[2] * enteredValues[3];
+                System.out.println("Result = " + result);
+            }
+
+        } while (i < 4);
+
+    }
+
+    private static boolean isCorrectDoubleValue(String value) {
+        boolean checkResult;
+
+        try {
+            Double enteredValue = Double.parseDouble(value);
+            checkResult = enteredValue >= 0;
+        } catch (NumberFormatException ex){
+            checkResult = false;
+        }
+        return checkResult;
     }
 }
